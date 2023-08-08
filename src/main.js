edb70b99329acdcc7b84c213d1a128aebe3fc4ae
davi <davijluna@gmail.com>
2023-08-07 22:08:44 -0300

    feat: adicionando elementos da forma dinâmica na página principal

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+
 const handleRates = ({rates}) => {
   const ratesEntries = Object.entries(rates)
   ratesEntries.forEach(([currency, rates]) => {
-    console.log(`${currency}:  ${rates}`)
+    const list = document.querySelector('#currency-list')
+    const li = document.createElement('li')
+    li.innerHTML = `${currency}: ${rates}`;
+    list.appendChild(li)
+    
   });
 }
