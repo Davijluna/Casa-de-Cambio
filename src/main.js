5bd0ab96208d1ea67d3a17983d57e619a7068c2d
davi <davijluna@gmail.com>
2023-08-08 01:15:54 -0300

    feat: dividindo duncionalidades

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 
+const renderRates = (currency, rates) => {
+  const list = document.querySelector('#currency-list')
+  const li = document.createElement('li')
+  li.innerHTML = `${currency}: ${rates}`;
+  list.appendChild(li)
+}
+
 const handleRates = ({rates}) => {
   const ratesEntries = Object.entries(rates)
   ratesEntries.forEach(([currency, rates]) => {
-    const list = document.querySelector('#currency-list')
-    const li = document.createElement('li')
-    li.innerHTML = `${currency}: ${rates}`;
-    list.appendChild(li)
-    
+    renderRates(currency, rates);
   });
 }
